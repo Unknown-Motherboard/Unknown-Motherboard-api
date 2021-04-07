e1fa7961e6aa173ad3deaae9edb8bbfc2fdeb3c4
mmacrides <mattmacrides@gmail.com>
2021-04-07 16:37:25 -0400

    half of part 2

---
+++ backend/Unkown.Motherboard.Domain/Catalog/Rating.cs
@@ -0,0 +1,27 @@
+using System;
+
+namespace Unkown.Motherboard.Domain.Catalog
+{
+    public class Rating
+    {
+        public int Star { get; set;}
+        public string UserName {get; set;}
+        public string Review {get; set;}
+
+        public Rating(int stars, string username, string review)
+        {
+            if (stars < 1 || stars > 5)
+            {
+                throw new ArgumentException("Star rating must be an integer: 1, 2, 3, 4, or 5.");
+            }
+
+            if(string.IsNullOrEmpty(username))
+            {
+                THROW NEW ArgumentException("UserName cannot be null.")
+            }
+
+            this.Star = stars;
+            this.UserName = username;
+            this.Review = review;
+    }
+}
