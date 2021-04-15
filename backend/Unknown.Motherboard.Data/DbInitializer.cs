b28b27a9f886f95614e253bd43910b211012b333
Matt Fogarty <mattfogarty13@gmail.com>
2021-04-15 19:57:22 -0400

    part 4

---
+++ backend/Unknown.Motherboard.Data/DbInitializer.cs
@@ -0,0 +1,23 @@
+using Unkown.Motherboard.Domain.Catalog;
+using Microsoft.Extensions.Logging;
+using System.Linq;
+
+namespace Unknown.Motherboard.Data
+{
+    public static class DbInitializer
+    {
+        public static void Initialize(StoreContext context, ILogger logger)
+        {
+            if(!context.Items.Any())
+            {
+                var items = new Item[]
+                {
+                    new Item("T-Shirt", "Ohio State Block O", "Nike", 39.99m),
+                    new Item("Shorts", "Casual shorts", "Nike", 49.99m)
+                };
+                context.Items.AddRange(items);
+                context.SaveChanges();
+            }
+        }
+    }
+}
