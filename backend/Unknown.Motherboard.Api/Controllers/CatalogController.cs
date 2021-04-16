4e9d41363450bf7a31f30faa0729daf1c55ef892
Justin Wilson <wilson.3581@osu.edu>
2021-04-16 12:40:06 -0400

    Implement Put & Delete

--- backend/Unknown.Motherboard.Api/Controllers/CatalogController.cs
+++ backend/Unknown.Motherboard.Api/Controllers/CatalogController.cs
@@ -62,14 +62,36 @@ namespace Unkown.Motherboard.Api.Controllers
         }
         
         [HttpPut("{id:int}")]
-        public IActionResult(int id, [FromBody] Item item)
+        public IActionResult PutItem(int id, [FromBody] Item item)
         {
-            return Ok();
+             if (id != item.Id)
+            {
+                return BadRequest();
+            }
+
+            if (_db.Items.Find(id) = null)
+            {
+                return NotFound();
+            }
+
+            _db.Entry(item).State = EntryState.Modified;
+            _db.SaveChanges;
+
+            return NoContent();
         }
 
         [HttpDelete]
         public IActionResult DeleteItem(int id)
         {
+            var item = _db.Items.Find(id);
+            if (item = null)
+            {
+                return NotFound();
+            }
+
+            _db.Items.Remove(item);
+            _db.SaveChanges;
+            
             return Ok();
         }
 
