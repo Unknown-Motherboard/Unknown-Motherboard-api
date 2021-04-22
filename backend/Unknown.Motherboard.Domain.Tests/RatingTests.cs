a82b418c485b461022c642c1eee0a85edde4a0eb
mmacrides <mattmacrides@gmail.com>
2021-04-22 12:53:51 -0400

    Part 1

---
+++ backend/Unknown.Motherboard.Domain.Tests/RatingTests.cs
@@ -0,0 +1,32 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Unkown.Motherboard.Domain.Catalog;
+
+
+namespace Unknown.Motherboard.Domain.Tests
+{
+    [TestClass]
+    public class RatingTests
+    {
+        [TestMethod]
+        public void Can_Create_New_Rating()
+        {
+            //Arrange
+            var rating = new Rating(1, "Mike", "Great fit!");
+
+            //Act - skip because there isn't action on constructor
+
+            //Assert
+            Assert.AreEqual(1, rating.Star);
+            Assert.AreEqual("Mike", rating.UserName);
+            Assert.AreEqual("Great fit!", rating.Review);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void Create_With_Bad_Start_Throws_Error()
+        {
+            var rating = new Rating(0, "Name", "Review");
+        }
+    }
+}
--- backend/Unknown.Motherboard.Domain/Orders/OrderItem.cs
+++ backend/Unknown.Motherboard.Domain/Orders/OrderItem.cs
@@ -5,8 +5,8 @@ namespace Unkown.Motherboard.Domain.Orders
     public class OrderItem
     {
         public int Id {get; set;}
-        public int Item {get; set;}
+        public Item Item {get; set;}
         public int Quantity {get; set;}
-        public int Price => Item.Price * Quantity;
+        public decimal Price => Item.Price * Quantity;
     }
 }
